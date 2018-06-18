57e3ad5f46ff45e1e50ba24edb27d4574cf4e4e3
Antonio Castilla <a.castillacastillo@gmail.com>
2018-06-18 23:51:47 +0200

    Ignorar (Archivos temporales)

--- ArbitroMetro/src/arbitrometro/ArbitroMetro.java
+++ ArbitroMetro/src/arbitrometro/ArbitroMetro.java
@@ -1,6 +1,5 @@
 package arbitrometro;
 
-import Herramientas.IO;
 import ventanas.InitDesing;
 
 /**
@@ -12,22 +11,16 @@ public class ArbitroMetro {
         // TODO code application logic here
         System.out.println("Arbitrometro to Profesional");
         //new InitDesing().setVisible(true);
-
+        
         Clasificacion c = new Clasificacion();
-
+        
         c.insertarEquipo("Dream Team");
         c.insertarEquipo("Urban Futsal");
-
-        System.out.println("Clasificación:\n" + c.toString());
-//        String csv = "Año,Marca,Modelo,Descripción,Precio\n"
-//                + "1997,Ford,E350,\"ac, abs, moon\",3000.00\n"
-//                + "1999,Chevy,\"Venture \"\"Extended Edition\"\"\",\"\",4900.00\n"
-//                + "1999,Chevy,\"Venture \"\"Extended Edition, Very Large\"\"\",,5000.00\n"
-//                + "1996,Jeep,Grand Cherokee,\"MUST SELL!\n"
-//                + "air, moon roof, loaded\",4799.00";
-//        
-        IO.escribeCsv("csv.csv", "");
-
+        
+        
+        System.out.println("Clasificación:\n"+c.toString());
+        
     }
-
+    
+    
 }
