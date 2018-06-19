76533bb10a2da1ec2e97e0c8f09958a55eccad4a
chelunike <chelunike@gmail.com>
2018-06-20 00:18:14 +0200

    Modificado GitIgnore

--- ArbitroMetro/src/arbitrometro/Jornada.java
+++ ArbitroMetro/src/arbitrometro/Jornada.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package arbitrometro;
 
 /**
@@ -10,5 +5,11 @@ package arbitrometro;
  * @author chelunike
  */
 public class Jornada {
+    //Atributos
+    
+    //Constructor
+    
+    //Metodo
+    
     
 }
---
+++ ArbitroMetro/src/arbitrometro/Liga.java
@@ -0,0 +1,21 @@
+package arbitrometro;
+import java.util.ArrayList;
+
+/**
+ * @author chelunike
+ */
+public class Liga {
+    //Atributos
+    private ArrayList<Jornada> jornadas;
+    
+    
+    //Constructor
+    public Liga(){
+        jornadas = new ArrayList<>();
+        
+    }
+    
+    //Metodo
+    
+    
+}
